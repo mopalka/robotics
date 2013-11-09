5022a0f038fd9fc3ad21adc1c95133a957dabd9a
mopalka <michal.opalka@pwr.wroc.pl>
2013-11-09 18:55:57 +0100

    Added moving rect.

--- interface.lisp
+++ interface.lisp
@@ -6,15 +6,37 @@
 (named-readtables:in-readtable :qt)
 
 (defclass canva ()
-  ()
+  ((if-hit :accessor if-hit :initform nil)
+  (rec-ang :accessor rec-ang :initform 0)
+  (rec-pos :accessor rec-pos :initform (#_new QPoint 100 100)))
   (:metaclass qt-class)
   (:qt-superclass "QWidget")
   (:override ("paintEvent" paint-event)
-	     ("mousePressEvent" mouse-press-event)))
+	     ("mousePressEvent" mouse-press-event)
+	     ("mouseReleaseEvent" mouse-release-event)
+	     ("mouseMoveEvent" mouse-move-event)))
 
 
 (defmethod mouse-press-event ((can canva) event)
-             (format t "~A" (#_pos event)))
+  (setf (if-hit can) (#_button event)))
+
+(defmethod mouse-release-event ((can canva) event)
+  (setf (if-hit can) nil))
+
+(defmethod mouse-move-event ((can canva) event) 
+ (if (enum= (if-hit can) (#_LeftButton "Qt"))
+      (progn
+	(#_setX (rec-pos can) (#_rx (#_pos event)))
+	(#_setY (rec-pos can) (#_ry (#_pos event)))
+
+	(format t "in: (~A,~A)~%" (#_rx (#_pos event)) (#_ry (#_pos event))))
+      (progn
+	(let ((radians (atan (- (float (#_bottom (#_rect can)) 1.0d0)
+				(#_y (#_pos event)))
+			     (#_x (#_pos event)))))
+	  (setf (rec-ang can)
+		(round (* radians (/ 180 pi)))))))
+	(#_update can))  
 
 
 (defmethod initialize-instance :after ((can canva) &key parent)
@@ -33,18 +55,23 @@
 
 
 (defun paint-base (can painter)
-  (with-objects ((rect (#_new QRect 100 100 50 50)))
-    (#_drawRect painter rect)))
+  (#_setPen painter (#_NoPen "Qt"))
+  (with-objects ((brush (#_new QBrush (#_blue "Qt") (#_SolidPattern "Qt"))))
+    (#_setBrush painter brush)
+    (let ((point (rec-pos can)))
+      (#_save painter)
+      (#_translate painter  (#_rx point)  (#_ry point))
+      (#_rotate painter (rec-ang can))
+      (with-objects ((rect (#_new QRect -25 -25 50 50)))   
+	(#_drawRect painter rect)
+	(#_restore painter)))))
 
 (defmethod paint-event ((can canva) paint-event)
   (with-objects ((painter (#_new QPainter can)))
     (#_setPen painter (#_black "Qt"))
-    (with-objects ((font (#_new QFont "Courier" 48 (#_Bold "QFont"))))
-      (#_setFont painter font))
-    ;;  (#_drawText painter (#_new QRect 0 0 400 400)
-    ;;		(#_AlignCenter "Qt") "Game Over") 
-    (format t "painting...")
     (paint-base can painter)
+    (with-objects ((rect (#_new QRect 0 0 50 50)))   
+      (#_drawRect painter rect))    
     (#_end painter)))
 
 (defclass main-window ()
